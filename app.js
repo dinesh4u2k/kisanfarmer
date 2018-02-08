e3f6f0696456c92c392b6c2ccf63ad1ffb6590bf
dinesh4u2k <dinesh4u2k@gmail.com>
2018-02-08 07:09:29 +0530

    extra

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use('/product', product);
 
 
 // Set Port
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
